ee976da39be5684eda58d5d7a70082c6d6a194d3
Greg Patrick <greg.hello@gmail.com>
2022-06-29 14:05:58 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Greg Patrick on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
